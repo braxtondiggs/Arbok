e600ea0feeb499e5477269abecac36a74b4e6a3a
braxtondiggs <braxtondiggs@gmail.com>
2014-12-17 16:11:07 -0500

    Remove Android Folder

--- platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes.dex : \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes/com/cymbit/quilava/BuildConfig.class \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes/com/cymbit/quilava/CordovaApp.class \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes/com/cymbit/quilava/R$attr.class \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes/com/cymbit/quilava/R$drawable.class \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes/com/cymbit/quilava/R$string.class \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes/com/cymbit/quilava/R$xml.class \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/classes/com/cymbit/quilava/R.class \
-/Users/braxtondiggs/Documents/Sites/Quilava/platforms/android/ant-build/dexedLibs/classes-1ba2c456d999c06dd3ea8ff75696195f.jar \

01095ec51344249983b9860ee884e495f1e718bb
braxtondiggs <braxtondiggs@gmail.com>
2015-04-24 17:43:00 -0400

    Add Player Settings

--- app/scripts/player_settings.js
+++ app/scripts/player_settings.js
@@ -1,9 +1,98 @@
 'use strict';
 angular.module('Quilava.controllers')
-	.controller('PlayerSettingsCtrl', ['$scope', '$rootScope', '$ionicLoading', '$localStorage', '$cordovaInAppBrowser', function($scope, $rootScope, $ionicLoading, $localStorage, $cordovaInAppBrowser) {
+	.controller('PlayerSettingsCtrl', ['$scope', '$ionicLoading', '$ionicPopup', '$ionicModal', '$localStorage', '$cordovaInAppBrowser', function($scope, $ionicLoading, $ionicPopup, $ionicModal, $localStorage, $cordovaInAppBrowser) {
 		$scope.$storage = $localStorage.$default({
 			initConfig: false
 		});
+		$scope.playerSettings = {
+			new: {
+			},
+			server: []
+		};
+		/*global Parse*/
+		$ionicModal.fromTemplateUrl('templates/modal/player_detail.html', {
+			scope: $scope,
+			animation: 'slide-in-up',
+			controller: 'PlayerSettingsDetailCtrl'
+		}).then(function(modal) {
+			$scope.modal = modal;
+		});
+		var user = Parse.User.current();
+		var relation = user.relation('userPlayer');
+		var query = relation.query();
+		query.equalTo('isSetup', true);
+		query.equalTo('isBox', true);
+		query.find({
+			success:function(list) {
+				$scope.players = list;
+				$scope.$apply();
+			}
+		});
+		$scope.addNewPlayer = function() {
+			function showPrompt() {
+				$ionicPopup.show({
+					template: '<div class="input"><input type="text" class="input__field" ng-model="playerSettings.new.objID"></div>',
+					title: 'Enter Player ID',
+					subTitle: 'The 8 character string that is visable on the TV screen.',
+					scope: $scope,
+					buttons: [
+						{ text: 'Cancel' },
+						{
+							text: '<b>Save</b>',
+							type: 'button-positive',
+							onTap: function(e) {
+								if (!$scope.playerSettings.new.objID) {
+									e.preventDefault();
+								} else {
+									$ionicLoading.show();
+									var Player = Parse.Object.extend('Player');
+									var query = new Parse.Query(Player);
+									query.equalTo('objectId', $scope.playerSettings.new.objID);
+									query.equalTo('isSetup', false);
+									query.equalTo('isBox', true);
+									query.find({
+										success: function(results) {
+											$ionicLoading.hide();
+											if (results.length) {
+												var user = Parse.User.current();
+												var relation = user.relation('userPlayer');
+												relation.add(results);
+    											user.save();
+												$scope.playerSettings.server = results[0];
+												$scope.name = $scope.playerSettings.server.get('name');
+												$scope.address = $scope.playerSettings.server.get('address');
+												$scope.image = ($scope.playerSettings.server.get('image'))?$scope.playerSettings.server.get('image')._url: null;
+												$scope.modal.show();
+											}else {
+												showPrompt();
+											}
+										}
+									});
+								}
+							}
+						}
+					]
+				});
+			}
+			showPrompt();
+		};
+		$scope.editPlayer = function(index) {
+			$scope.playerSettings.server = $scope.players[index];
+			$scope.name = $scope.playerSettings.server.get('name');
+			$scope.address = $scope.playerSettings.server.get('address');
+			$scope.image = ($scope.playerSettings.server.get('image'))?$scope.playerSettings.server.get('image')._url: null;
+			$scope.modal.show();
+		};
+		$scope.deletePlayer = function(index) {
+			PubNub.ngPublish({
+				channel: $scope.player[index].id,
+				message: {'player_delete': $scope.player[index].id}
+			});
+			$scope.player[index].destroy();
+		};
+		$scope.closePlayerEdit = function() {
+			$scope.modal.hide();
+		};
 		$scope.getStarted = function() {
 			$scope.$storage.initConfig = true;
 		};
---
+++ app/scripts/player_settings_details.js
@@ -0,0 +1,62 @@
+'use strict';
+angular.module('Quilava.controllers')
+	.controller('PlayerSettingsDetailCtrl', ['$scope', '$cordovaCamera', '$ionicLoading', 'PubNub', function($scope, $cordovaCamera, $ionicLoading, PubNub) {
+		/*global Parse*/
+		$scope.psd = {};
+		$scope.submitForm = function() {
+			$scope.psd.geoCode($scope.address, function(geocode) {
+				$scope.playerSettings.server.set('isSetup', true);
+				$scope.playerSettings.server.set('userId', Parse.User.current());
+				$scope.playerSettings.server.set('latlng', new Parse.GeoPoint({latitude: geocode.latitude, longitude: geocode.longitude}));
+	    		$scope.playerSettings.server.save();
+	    		$scope.modal.hide();
+	    		$ionicLoading.show({
+					template: 'Save was succesful...',
+					duration: 2000
+				});
+				PubNub.ngPublish({
+					channel: $scope.playerSettings.new.objID,
+					message: {'player_update': $scope.playerSettings.server}
+				});
+			});
+		};
+		$scope.psd.geoCode = function(address, callback) {
+			/*global GeocoderJS*/
+			var googleGeocoder = new GeocoderJS.createGeocoder({'provider': 'google'});
+		    googleGeocoder.geocode(address, function(result) {
+		        callback(result[0]);
+		    });
+		};
+		$scope.inputChange = function(name, input) {
+			$scope.playerSettings.server.set(name, input);
+		};
+		$scope.clearImage = function() {
+			$scope.playerSettings.server.unset('image');
+			$scope.image = null;
+		};
+		$scope.getImage = function() {
+			/* global Camera*/
+			var pickerOptions = {
+				destinationType: Camera.DestinationType.DATA_URL,
+				sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+				quality: 50,
+				EncodingType: Camera.EncodingType.PNG,
+				MediaType: Camera.MediaType.PICTURE,
+				targetWidth: 150,
+				targetHeight: 150
+			};
+			$cordovaCamera.getPicture(pickerOptions).then(function(imageData) {
+				var file = new Parse.File($scope.playerSettings.new.objID + '.png', {
+					base64: imageData
+				});
+				$scope.playerSettings.server.set('image', file);
+				$scope.image = file;
+			}, function() {
+				$ionicLoading.hide();
+				$ionicLoading.show({
+					template: 'Error, could not load photo...',
+					duration: 2000
+				});
+			});
+		};
+	}]);

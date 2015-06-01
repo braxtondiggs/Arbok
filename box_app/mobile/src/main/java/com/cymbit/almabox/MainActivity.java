750397b82f03ff4f8c02bdf649ecae4beaf90055
Braxton Diggs <braxtondiggs@gmail.com>
2015-06-01 01:34:34 -0400

    Android Development

--- box_app/mobile/src/main/java/com/cymbit/almabox/MainActivity.java
+++ box_app/mobile/src/main/java/com/cymbit/almabox/MainActivity.java
@@ -1,5 +1,8 @@
 package com.cymbit.almabox;
 
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +10,9 @@ import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.parse.GetCallback;
 import com.parse.Parse;
@@ -18,26 +24,23 @@ import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
-    //WebView myWebView = (WebView) findViewById(R.id.webview);
-    //WebSettings webSettings = myWebView.getSettings();
+    WebView mWebView = (WebView) findViewById(R.id.webview);
+    TextView loadingText = (TextView) findViewById(R.id.loadingText);
+    RelativeLayout loadingContainer = (RelativeLayout) findViewById(R.id.loadingContainer);
+
     private View mDecorView;
+    private DownloadManager downloadManager;
+    private long downloadReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDecorView = getWindow().getDecorView();
         mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
-        //webSettings.setJavaScriptEnabled(true);
-        //webSettings.setUseWideViewPort(true);
-        //webSettings.setLoadWithOverviewMode(true);
-        //myWebView.loadUrl("http://quilava.herokuapp.com/#/player/box");*/
-
-
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "GxJOG4uIVYMnkSuRguq8rZhTAW1f72eOQ2uXWP0k", "oh8eC3w1vnRfBHpAaRljovwzVNaQJnrbh65ei7Wf");
-        /*ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();*/
+
         setContentView(R.layout.activity_main);
         checkVersion();
 
@@ -61,7 +64,7 @@ public class MainActivity extends ActionBarActivity {
     }
     public void checkVersion() {
         final String versionName = BuildConfig.VERSION_NAME;
-
+        loadingText.setText("Checking for updates... Please Wait");
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Server");
         query.getFirstInBackground(new GetCallback<ParseObject>() {
             public void done(ParseObject serverInfo, ParseException e) {
@@ -79,9 +82,34 @@ public class MainActivity extends ActionBarActivity {
         });
     }
     public void loadPlayer() {
-
+        WebSettings webSettings = mWebView.getSettings();
+        loadingText.setText("Please Wait...");
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        mWebView.loadUrl("http://quilava.herokuapp.com/#/player/box");
+        mWebView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                loadingText.setText("");
+                loadingContainer.setVisibility(View.GONE);
+            }
+        });
     }
     public void updatePlayer(String url) {
+        downloadManager = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
+        Uri Download_Uri = Uri.parse(url);
 
+        loadingText.setText("Updating Player... Please Wait");
+
+        DownloadManager.Request r = new DownloadManager.Request(Download_Uri);
+
+        r.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "AlmaBox.apk");
+        r.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+        downloadReference = downloadManager.enqueue(r);
+
+        //DownloadManager dm = (DownloadManager) activity.getSystemService(Context.DOWNLOAD_SERVICE);
+        //SharedPreferences mSharedPref = activity.getSharedPreferences("package", Context.MODE_PRIVATE);
+        //mSharedPref.edit().putLong("downloadID", dm.enqueue(r)).commit();
     }
 }

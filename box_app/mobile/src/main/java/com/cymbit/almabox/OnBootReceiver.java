05ce9f43e8fe8d0b7047394ec39d09430a47afa0
Braxton Diggs <braxtondiggs@gmail.com>
2015-07-01 02:38:15 -0400

    Daily Check-in

--- box_app/mobile/src/main/java/com/cymbit/almabox/OnBootReceiver.java
+++ box_app/mobile/src/main/java/com/cymbit/almabox/OnBootReceiver.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.cymbit.almabox.pages.IntroPage;
+
 public class OnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("OnBootReceiver", "Hi, Mom!");
-        Intent mainIntent = new Intent(context, SetupActivity.class);
+        Intent mainIntent = new Intent(context, IntroPage.class);
         context.startActivity(mainIntent);
     }
 }
---
+++ box_app/mobile/src/main/java/com/cymbit/almabox/pages/IntroPage.java
@@ -0,0 +1,15 @@
+package com.cymbit.almabox.pages;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.cymbit.almabox.R;
+
+public class IntroPage extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_activity);
+    }
+}
